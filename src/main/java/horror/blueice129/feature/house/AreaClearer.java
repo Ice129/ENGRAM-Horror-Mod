4999a1c817eeda8776aff6c387a7323614931be0
ice129 <jatkinson15@pm.me>
2026-03-14 17:03:19 +0000

    Enhance AreaClearer functionality to remove foliage and place torches with surface checks

--- src/main/java/horror/blueice129/feature/house/AreaClearer.java
+++ src/main/java/horror/blueice129/feature/house/AreaClearer.java
@@ -1,17 +1,23 @@
 package horror.blueice129.feature.house;
 
+import horror.blueice129.utils.BlockTypes;
 import horror.blueice129.utils.SurfaceFinder;
+import horror.blueice129.utils.TorchPlacer;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 
 public class AreaClearer {
 
     private static final int CLEAR_RADIUS = 20;
     private static final int TORCH_RADIUS = 22;
+    private static final Random random = Random.create();
 
     public static void clearArea(ServerWorld world, BlockPos center) {
+        int y = SurfaceFinder.findPointSurfaceY(world, center.getX(), center.getZ(), true, true, true);
+
         BlockPos[] trees = SurfaceFinder.findTreePositions(world, center, CLEAR_RADIUS);
         for (BlockPos treeBase : trees) {
             for (BlockPos logPos : SurfaceFinder.getTreeLogPositions(world, treeBase)) {
@@ -19,10 +25,33 @@ public class AreaClearer {
             }
         }
 
-        // TODO: scan each column within CLEAR_RADIUS for leaves, tall grass, and flowers and remove them
+        for (int x = -CLEAR_RADIUS; x <= CLEAR_RADIUS; x++) {
+            for (int z = -CLEAR_RADIUS; z <= CLEAR_RADIUS; z++) {
+                for (int dy = -3; dy <= 3; dy++) {
+                    BlockPos pos = new BlockPos(center.getX() + x, y + dy, center.getZ() + z);
+                    Block block = world.getBlockState(pos).getBlock();
+                    if (BlockTypes.isFoliage(block, false)) {
+                        world.setBlockState(pos, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL);
+                    }
+                }
+            }
+        }
+
     }
 
     public static void placeTorches(ServerWorld world, BlockPos center) {
-        // TODO: walk the perimeter at TORCH_RADIUS, find solid surface positions, place torches on top
+        for (int x = -TORCH_RADIUS; x <= TORCH_RADIUS; x++) {
+            for (int z = -TORCH_RADIUS; z <= TORCH_RADIUS; z++) {
+                if (random.nextFloat() < 0.07f) {
+                    int posy = SurfaceFinder.findPointSurfaceY(world, center.getX() + x, center.getZ() + z, true,
+                            true, true);
+                    
+                    if (posy != -1) {
+                        BlockPos pos = new BlockPos(center.getX() + x, posy + 1, center.getZ() + z);
+                        TorchPlacer.placeTorch(world, pos, random, null);
+                    }
+                }
+            }
+        }
     }
 }
